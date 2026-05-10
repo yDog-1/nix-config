066d21c8a15b25636c019f3508633e4d65d623f2
yDog <ydog-1@outlook.jp>
2026-05-10 19:17:22 +0900

    fix(hyprland): fix workspace config that had wrong syntax

--- modules/home/desktop/hyprland.nix
+++ modules/home/desktop/hyprland.nix
@@ -16,16 +16,16 @@
       ];
 
       workspace = [
-        "1,m[DP-3]"
-        "2,m[DP-3]"
-        "3,m[DP-3]"
-        "4,m[DP-3]"
-        "5,m[DP-3]"
-        "6,m[HDMI-A-1]"
-        "7,m[HDMI-A-1]"
-        "8,m[HDMI-A-1]"
-        "9,m[HDMI-A-1]"
-        "10,m[HDMI-A-1]"
+        "1, monitor:DP-3, default:true"
+        "2, monitor:DP-3"
+        "3, monitor:DP-3"
+        "4, monitor:DP-3"
+        "5, monitor:DP-3"
+        "6, monitor:HDMI-A-1, default:true"
+        "7, monitor:HDMI-A-1"
+        "8, monitor:HDMI-A-1"
+        "9, monitor:HDMI-A-1"
+        "10, monitor:HDMI-A-1"
       ];
 
       env = [
