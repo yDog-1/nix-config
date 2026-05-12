a5deebc14fed4dd31bf823cb89455918c3aa1272
yDog <ydog-1@outlook.jp>
2026-05-12 22:39:39 +0900

    fix(mounts): change to uuid instead of device path

--- modules/nixos/mounts.nix
+++ modules/nixos/mounts.nix
@@ -1,12 +1,12 @@
 {
   fileSystems = {
-    "/mnt/sda1" = {
-      device = "/dev/sda1";
+    "/mnt/data/main" = {
+      device = "/dev/disk/by-uuid/99d4b666-a3b4-4bd8-b5f2-debf556ce30f";
       fsType = "auto";
     };
 
-    "/mnt/sdc2" = {
-      device = "/dev/sdc2";
+    "/mnt/data/second" = {
+      device = "/dev/disk/by-uuid/6295dbe7-f8ec-4cd1-ac5a-880fcbbb4cae";
       fsType = "auto";
     };
   };
