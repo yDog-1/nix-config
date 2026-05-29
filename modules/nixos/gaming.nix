2755d6c12eff13cafd9d55d1218849284f61b306
yDog <ydog-1@outlook.jp>
2026-05-29 11:35:27 +0900

    feat(gaming): switch to remote play on sunshine from steam link

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,8 +1,19 @@
 {
+  inputs,
+  pkgs,
+  ...
+}: {
   hardware.xpadneo.enable = true;
 
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true;
+  };
+
+  services.sunshine = {
+    enable = true;
+    autoStart = false;
+    capSysAdmin = false;
+    openFirewall = true;
+    package = inputs.nixpkgs-25-05.legacyPackages.${pkgs.system}.sunshine;
   };
 }
