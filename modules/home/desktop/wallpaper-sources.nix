9d129fd76e1a1e954dcd8b8908bdd08b22a7d802
yDog <ydog-1@outlook.jp>
2026-06-07 14:23:07 +0900

    fix(wallpaper): remove default wallpaper source

--- modules/home/desktop/wallpaper-sources.nix
+++ modules/home/desktop/wallpaper-sources.nix
@@ -1,7 +1,7 @@
 [
-  {
-    name = "pexels-photo-3075993.jpeg";
-    url = "https://images.pexels.com/photos/3075993/pexels-photo-3075993.jpeg";
-    hash = "sha256-/D8Mq8R/Jss9EOZ74qs8p+ShoUcB6z8z7EPCFxK8pbA=";
-  }
+  # {
+  #   name = "pexels-photo-3075993.jpeg";
+  #   url = "https://images.pexels.com/photos/3075993/pexels-photo-3075993.jpeg";
+  #   hash = "sha256-/D8Mq8R/Jss9EOZ74qs8p+ShoUcB6z8z7EPCFxK8pbA=";
+  # }
 ]
