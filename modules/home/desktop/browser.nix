c0ba4670da0466e12551780a46dc9c71058e65a8
yDog <ydog-1@outlook.jp>
2026-06-08 21:16:36 +0900

    fix(browser): set Vivaldi as default browser Previous Home Manager migration made Firefox the default

--- modules/home/desktop/browser.nix
+++ modules/home/desktop/browser.nix
@@ -7,4 +7,16 @@
     enable = true;
     configPath = ".mozilla/firefox";
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "vivaldi-stable.desktop";
+      "application/xhtml+xml" = "vivaldi-stable.desktop";
+      "x-scheme-handler/http" = "vivaldi-stable.desktop";
+      "x-scheme-handler/https" = "vivaldi-stable.desktop";
+      "x-scheme-handler/about" = "vivaldi-stable.desktop";
+      "x-scheme-handler/unknown" = "vivaldi-stable.desktop";
+    };
+  };
 }
