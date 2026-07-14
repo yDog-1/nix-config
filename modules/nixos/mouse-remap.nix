c41471a3b7a63de2731394d99207bf4f92e45755
yDog <ydog-1@outlook.jp>
2026-07-14 21:36:07 +0900

    fix(mouse-remap): run evdevremapkeys with Python 3.13

--- modules/nixos/mouse-remap.nix
+++ modules/nixos/mouse-remap.nix
@@ -1,19 +1,32 @@
 {
+  lib,
+  pkgs,
+  ...
+}: let
+  settings = {
+    devices = [
+      {
+        input_name = "Getech HUGE TrackBall";
+        output_name = "ELECOM HUGE Remapped";
+        remappings = {
+          BTN_TASK = ["BTN_MIDDLE"];
+        };
+      }
+    ];
+  };
+  configFile = (pkgs.formats.yaml {}).generate "evdevremapkeys-config.yaml" settings;
+  evdevremapkeys = pkgs.evdevremapkeys.override {
+    python3Packages = pkgs.python313Packages;
+  };
+in {
   services.evdevremapkeys = {
     enable = true;
-    settings = {
-      devices = [
-        {
-          input_name = "Getech HUGE TrackBall";
-          output_name = "ELECOM HUGE Remapped";
-          remappings = {
-            BTN_TASK = ["BTN_MIDDLE"];
-          };
-        }
-      ];
-    };
+    inherit settings;
   };
 
   # evdevremapkeys creates virtual input devices through /dev/uinput.
-  systemd.services.evdevremapkeys.serviceConfig.SupplementaryGroups = ["uinput"];
+  systemd.services.evdevremapkeys.serviceConfig = {
+    ExecStart = lib.mkForce "${lib.getExe evdevremapkeys} --config-file ${configFile}";
+    SupplementaryGroups = ["uinput"];
+  };
 }
