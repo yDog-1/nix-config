2bc204ce44cf2294058f36ffb9723e22a9ccb71b
yDog <ydog-1@outlook.jp>
2026-07-25 16:24:13 +0900

    chore(dotfiles): remove chezmoi configuration

--- modules/home/cli/packages.nix
+++ modules/home/cli/packages.nix
@@ -19,7 +19,6 @@
 
     imagemagick
     ghostscript
-    chezmoi
 
     go
     nodejs
--- nput/ydog-1/.config/nvim/lua/filetype.lua
+++ nput/ydog-1/.config/nvim/lua/filetype.lua
@@ -2,20 +2,4 @@ vim.filetype.add({
 	extension = {
 		astro = "astro",
 	},
-	pattern = {
-		["${HOME}/.local/share/chezmoi/.*"] = {
-			function(path, buf)
-				if path:match("/dot_") then
-					return vim.filetype.match({
-						filename = path:gsub("/dot_", "/."),
-						buf = buf,
-					})
-				end
-			end,
-			-- テーブルの第二要素でpriorityを最低にしておくと、フォールバック相当になる
-			{ priority = -math.huge },
-		},
-		-- ${HOME}/.local/share/chezmoi/dot_config/yamllint/config を yamlとして扱う
-		["${HOME}/.local/share/chezmoi/dot_config/yamllint/config"] = "yaml",
-	},
 })
