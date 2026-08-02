73774419dcba042684f17f5c91db6923c64658fd
yDog <ydog-1@outlook.jp>
2026-08-02 10:55:10 +0900

    fix(nput): symlink config files individually

--- nput/ydog-1/default.nix
+++ nput/ydog-1/default.nix
@@ -6,25 +6,13 @@ _: let
     }) (builtins.attrNames entries));
 
   configRoot = ./.config;
-  specialConfigDirectories = ["nvim"];
-  configDirectories =
-    builtins.filter (name:
-      (builtins.readDir configRoot).${name} == "directory") (builtins.attrNames (builtins.readDir configRoot));
-
-  genericConfigEntries = builtins.listToAttrs (map (name: {
-    name = ".config/${name}";
-    value.src = configRoot + "/${name}";
-  }) (builtins.filter (name: !(builtins.elem name specialConfigDirectories)) configDirectories));
-
-  nvimSources = import ./file-tree.nix ./.config/nvim;
-  nvimEntries = prefixEntries ".config/nvim" nvimSources;
+  configEntries = prefixEntries ".config" (import ./file-tree.nix configRoot);
 in
-  genericConfigEntries
-  // nvimEntries
+  configEntries
   // {
     # Lazy.nvim updates this lockfile itself, so it must not be a symlink.
     ".config/nvim/lazy-lock.json" =
-      nvimEntries.".config/nvim/lazy-lock.json"
+      configEntries.".config/nvim/lazy-lock.json"
       // {
         method = "copy";
       };
