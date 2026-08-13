fa188060121cbd060086fe9aea89aa7e975eeee4
yDog <ydog-1@outlook.jp>
2026-08-13 19:55:42 +0900

    chore(nix): remove Garnix cache

--- flake.nix
+++ flake.nix
@@ -4,13 +4,11 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "https://cache.garnix.io"
       "https://cache.numtide.com"
       "https://ydog-1-nur.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       "ydog-1-nur.cachix.org-1:gw4tWFtMdLnDn2k1EMrkgUrheq8/zi8mjPQKto5PyDs="
     ];
