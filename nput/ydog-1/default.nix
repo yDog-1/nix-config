1bc868466b6985b6566e63b9330714eca54ee895
yDog <ydog-1@outlook.jp>
2026-08-13 15:18:06 +0900

    fix(nput): symlink lazy lockfile

--- nput/ydog-1/default.nix
+++ nput/ydog-1/default.nix
@@ -10,23 +10,15 @@
       value = entries.${name};
     }) (builtins.attrNames entries));
 
-  storeConfigEntries = prefixEntries ".config" (import ./file-tree.nix ./.config);
+  configFileEntries = prefixEntries ".config" (import ./file-tree.nix ./.config);
   configEntries =
     builtins.mapAttrs (name: _: {
       # Keep dotfiles outside the Nix store so they survive garbage collection.
       src = inputs.nput.lib.mkOutOfStoreSymlink "${flakePath}/nput/ydog-1/.config/${pkgs.lib.removePrefix ".config/" name}";
     })
-    storeConfigEntries;
+    configFileEntries;
 in
   configEntries
-  // {
-    # Lazy.nvim updates this lockfile itself, so it must not be a symlink.
-    ".config/nvim/lazy-lock.json" =
-      storeConfigEntries.".config/nvim/lazy-lock.json"
-      // {
-        method = "copy";
-      };
-  }
   // {
     ".gitconfig".src = inputs.nput.lib.mkOutOfStoreSymlink "${flakePath}/nput/ydog-1/.gitconfig";
   }
