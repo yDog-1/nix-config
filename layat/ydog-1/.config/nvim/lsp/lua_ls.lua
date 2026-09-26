4aa19b80c01f601591699fde6bb88835e6d01adb
yDog <ydog-1@outlook.jp>
2026-09-26 20:22:37 +0900

    feat(nvim): include lazy plugins in lua_ls workspace

--- layat/ydog-1/.config/nvim/lsp/lua_ls.lua
+++ layat/ydog-1/.config/nvim/lsp/lua_ls.lua
@@ -1,3 +1,17 @@
+local lazy_root = require("lazy.core.config").options.root
+
+local library = vim.api.nvim_get_runtime_file("lua", true)
+for name, type in vim.fs.dir(lazy_root) do
+	if type == "directory" then
+		library[#library + 1] = vim.fs.joinpath(lazy_root, name, "lua")
+	end
+end
+vim.list_extend(library, {
+	"${3rd}/luv/library",
+	"${3rd}/busted/library",
+	"${3rd}/luassert/library",
+})
+
 ---@type vim.lsp.Config
 return {
 	settings = {
@@ -8,11 +22,7 @@ return {
 				path = { "?.lua", "?/init.lua" },
 			},
 			workspace = {
-				library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
-					"${3rd}/luv/library",
-					"${3rd}/busted/library",
-					"${3rd}/luassert/library",
-				}),
+				library = library,
 				checkThirdParty = "Disable",
 			},
 		},
